d9146cd2b6d62470e5c2df81c37f76e537f521e9
Sopfiee <sonyakrohina@yandex.ru>
2024-02-10 00:27:00 +0300

    gitignore

--- Main.py
+++ Main.py
@@ -1,8 +1,11 @@
 from kivymd.app import MDApp
 from kivymd.uix.label import MDLabel
 
+
 # класс приложения
 class Achievement(MDApp):
     def build(self):
         return MDLabel(text="Hello, super hero", halign="center")
 
+
+Achievement().run()
